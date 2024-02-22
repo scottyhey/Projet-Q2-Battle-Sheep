1b71c39465aed202b583af1625dcab5b186a18c8
Azerty131131 <159807970+Azerty131131@users.noreply.github.com>
2024-02-22 12:38:37 +0100

    Add files via upload

---
+++ Fonction_Jeu.py
@@ -0,0 +1,64 @@
+def get_distance():
+    """Get the distance (distane between two entites)"""
+
+def create_map_dictio():
+    """create a dictionary with every usefull informations about the map creation from the #map file#
+
+    notes
+    -----
+    the file should be in ./maps/
+
+    version
+    -------
+    specification: Heynen Scott-Socrate (v1 20/02/24)
+    """
+
+def is_game_over():
+    """check if game is over
+
+    returns
+    -------
+    true if the game is over, false otherwise (bool)
+
+    version
+    -------
+    specification : Heynen Scott-Socrate (v1 20/02/24)"""
+
+
+def display_map(map,player,grass):
+    """display the map from the diction""" 
+
+def try_spawn_sheep(player_nbm):
+    """Spawn a sheep if it's needed"""
+
+def look_a_barrier(coordonate):
+    """Look if a sheep can spawn or move on a case."""
+    #think about specifiate the type of the thing that obstructs
+
+def get_seed (coordonate):
+    """Verify if therer are a seed on the case"""
+
+def growth_grass():
+    """Growth all the grass, in fact add 1 to the life_stats"""
+
+def grass_propagation (coordonate):
+    """Propage the grass around"""
+
+def delete_emoji (coordonate):
+    """delete the emoji we need """
+
+def attack_sheep(coordonate):
+    """Attack a sheep if he is near enough to be attack"""
+
+def move_sheep (old_coordonate,new_coordonate):
+    """Move a sheep if he want to move or if i was attack"""
+
+def sheep_graze(coordonate):
+    """Graze a grass if the sheep is on this case and the case"""
+    
+map={'rock':{'rock_1':(1,2),'rock_2':(2,5)}}
+
+for rocks in map['rock']:
+    my_tuples=map['rock'][rocks]
+
+#get_sheep_life:()
